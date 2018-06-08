fb7de8245cb1f3bde3588440ad8b35ba1a382f67
Takezou.Shinmen <ec2-user@ip-172-31-17-29.us-east-2.compute.internal>
2018-06-08 06:50:31 +0000

    post

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -30,7 +30,7 @@ class MicropostsController extends Controller
             return view('welcome');
         }
     }
-        public function store(Request $request)
+    public function store(Request $request)
     {
         $this->validate($request, [
             'content' => 'required|max:191',
@@ -41,15 +41,6 @@ class MicropostsController extends Controller
         ]);
 
         return redirect()->back();
-    }
-        public function destroy($id)
-    {
-        $micropost = \App\Micropost::find($id);
-
-        if (\Auth::user()->id === $micropost->user_id) {
-            $micropost->delete();
-        }
-
-        return redirect()->back();
-    }
+    }    
 }
+
