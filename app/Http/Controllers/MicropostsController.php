a5a48c9e8de253a49bbd491918e159e40b010465
Takezou.Shinmen <ec2-user@ip-172-31-17-29.us-east-2.compute.internal>
2018-06-11 04:20:16 +0000

    follow, unfollow

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -41,6 +41,16 @@ class MicropostsController extends Controller
         ]);
 
         return redirect()->back();
+    }   
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::user()->id === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return redirect()->back();
     }    
 }
 
